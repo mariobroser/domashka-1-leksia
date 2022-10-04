09c12a43cb66c2960db7b6ab8b2349a19ce305e5
unknown <erbolatsuleev@mail.ru>
2022-10-04 21:58:48 +0600

    domashka3

---
+++ Домашка3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первую цифру ");
+string a1 = Console.ReadLine();
+int a = int.Parse(a1);
+Console.Write("Введите вторую цифру ");
+string b1 = Console.ReadLine();
+int b = int.Parse(b1);
+Console.Write("Введите третью цифру ");
+string c1 = Console.ReadLine();
+int c = int.Parse(c1);
+int max = a;
+
+if(a%2 == 0) {Console.WriteLine(a);};
+if(b%2 == 0) {Console.WriteLine(b);};
+if(c%2 == 0) {Console.WriteLine(c);};
