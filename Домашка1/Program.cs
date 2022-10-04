23a8d20976a64e7351164b38740c8ee538edbfa4
unknown <erbolatsuleev@mail.ru>
2022-10-04 21:47:23 +0600

    domashka1

---
+++ Домашка1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первую цифру ");
+string a1 = Console.ReadLine();
+int a = int.Parse(a1);
+Console.Write("Введите вторую цифру ");
+string b1 = Console.ReadLine();
+int b = int.Parse(b1);
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+
+Console.Write("max= ");
+Console.Write(max);
